f24c9dbaa188057c693406ae9ef6194ad45c5dcd
Carcharodon80 <karna241280@gmail.com>
2022-04-29 10:55:08 +0300

    3. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new ConsoleProgress());
+        thread.start();
+        Thread.sleep(3000);
+        thread.interrupt();
+    }
+
+    @Override
+    public void run() {
+        String[] array = new String[]{"|", "/", "-", "\\"};
+        while (!Thread.currentThread().isInterrupted()) {
+            for (String dash : array) {
+                System.out.print("\rLoading... " + dash);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    return;
+                }
+            }
+        }
+    }
+}
