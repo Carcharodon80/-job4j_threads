f72f103729ee2775bcabd627165e1a930699aaf6
Carcharodon80 <karna241280@gmail.com>
2022-05-05 14:13:00 +0300

    temp

---
+++ src/main/java/ru/job4j/ref/UserCache.java
@@ -0,0 +1,28 @@
+package ru.job4j.ref;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@ThreadSafe
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<Integer, User>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        List<User> listUsers = new ArrayList<>();
+        for (User tempUser : users.values()) {
+            listUsers.add(User.of(tempUser.getName()));
+        }
+        return listUsers;
+    }
+}
