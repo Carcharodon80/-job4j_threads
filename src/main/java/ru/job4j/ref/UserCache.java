aa5be9b5b48dd745906f1ad73b439128a9ddd847
Carcharodon80 <karna241280@gmail.com>
2022-05-10 23:17:54 +0300

    temp

--- src/main/java/ru/job4j/ref/UserCache.java
+++ src/main/java/ru/job4j/ref/UserCache.java
@@ -1,11 +1,14 @@
 package ru.job4j.ref;
 
+import net.jcip.annotations.NotThreadSafe;
+import net.jcip.annotations.ThreadSafe;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
-@ThreadSafe
+@NotThreadSafe
 public class UserCache {
     private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<Integer, User>();
     private final AtomicInteger id = new AtomicInteger();
