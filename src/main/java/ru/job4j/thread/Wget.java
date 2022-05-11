8b3d4940ac1c9d165151b3af2a9b9e1599ce63ac
Carcharodon80 <karna241280@gmail.com>
2022-05-11 14:01:08 +0300

    4. Скачивание файла с ограничением. [#144271 #297576]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -9,47 +9,70 @@ import java.time.*;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String outFile;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String outFile) {
         this.url = url;
         this.speed = speed;
+        this.outFile = outFile;
     }
 
     /**
-     * метод читает файл из сети и пишет его в "out.txt"
-     * размер буффера == скорость скачивания за секунду,
-     * если буфер скачался/записался быстрее, чем за секунду - делаем задержку
+     * метод читает файл из сети и пишет его в outFile,
+     * считаем кол-во прочитанных/записанных байт, если превышает speed,
+     * и затраченное на это время меньше секунды - делаем задержку.
      */
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream out = new FileOutputStream("out.txt")) {
-            byte[] array = new byte[speed];
+             FileOutputStream out = new FileOutputStream(outFile)) {
+            byte[] array = new byte[1024];
             int bytesRead;
+            long bytesWrited = 0;
             Instant start = Instant.now();
-            while ((bytesRead = in.read(array, 0, speed)) != -1) {
+            while ((bytesRead = in.read(array, 0, 1024)) != -1) {
                 out.write(array, 0, bytesRead);
-                Instant end = Instant.now();
-                long time = Duration.between(start, end).toMillis();
-                if (time < 1000) {
-                    Thread.sleep(1000 - time);
+                bytesWrited += bytesRead;
+                if (bytesWrited >= speed) {
+                    long deltaTime = Duration.between(start, Instant.now()).toMillis();
+                    if (deltaTime < 1000) {
+                        Thread.sleep(1000 - deltaTime);
+                    }
+                    bytesWrited = 0;
+                    start = Instant.now();
                 }
-                start = Instant.now();
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
+    }
 
+    /**
+     * метод проверяет кол-во аргументов
+     */
+    private static void isValidArgs(int argsNum) {
+        if (argsNum != 3) {
+            System.out.println("Проверьте параметры:");
+            System.out.println("1 - URL скачиваемого файла");
+            System.out.println("2 - максимальная скорость скачивания (байт/сек");
+            System.out.println("3 - имя файла для записи");
+            throw new IllegalArgumentException();
+        }
     }
 
     /**
      * @param args - 1 аргумент - URL скачиваемого файла,
      *             2 аргумент - максимальное кол-во байт, которое можно скачать за 1 секунду
+     *             3 аргумент - имя выходного файла
      */
     public static void main(String[] args) throws InterruptedException {
+        isValidArgs(args.length);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String outFile = args[2];
+        Thread wget = new Thread(new Wget(url, speed, outFile));
         wget.start();
         wget.join();
     }
