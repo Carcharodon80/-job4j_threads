910a1df78eb8b52d5ae1b800fe7ab9821364dce9
Carcharodon80 <karna241280@gmail.com>
2022-06-16 00:37:36 +0300

    1. Реализовать ThreadPool [#1099]

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -5,10 +5,15 @@ import ru.job4j.SimpleBlockingQueue;
 import java.util.LinkedList;
 import java.util.List;
 
+
 public class ThreadPool {
     private final List<Thread> threads = new LinkedList<>();
     private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(10);
 
+    /**
+     * создает список нитей (размер - кол-во ядер),
+     * каждая нить постоянно пытается получить и запустить задачу из tasks (пока не будет прервана)
+     */
     public ThreadPool() {
         int size = Runtime.getRuntime().availableProcessors();
         for (int i = 0; i < size; i++) {
@@ -24,23 +29,43 @@ public class ThreadPool {
                     }
             ));
         }
+        for (Thread t : threads) {
+            t.start();
+        }
     }
 
-    public void work(Runnable job) {
+    /**
+     * добавляет задачи в tasks (а оттуда их постоянно забирают в работу нити из threads)
+     */
+    public synchronized void work(Runnable job) {
         try {
             tasks.offer(job);
-            for (Thread thread : threads) {
-                thread.notify();
-            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * прерывает все нити (несмотря на наличие задач в очереди)
+     */
     public void shutdown() {
-        for (Thread thread : threads) {
-            thread.interrupt();
-        }
+            for (Thread thread : threads) {
+                thread.interrupt();
+            }
     }
 
+    /**
+     * пример работы (для себя)
+     * ! - pool.shutdown - экстренная остановка (к этому времени некоторые задачи еще в очереди)
+     */
+    public static void main(String[] args) {
+        ThreadPool pool = new ThreadPool();
+        for (int i = 0; i < 100; i++) {
+            int finalI = i;
+            pool.work(new Thread(
+                    () -> System.out.println(Thread.currentThread().getName() + " Task " + finalI)
+            ));
+        }
+        pool.shutdown();
+    }
 }
