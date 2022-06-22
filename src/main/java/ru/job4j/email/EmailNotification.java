a79b93f6608deea1427e4a54b9d7adf478e748be
Carcharodon80 <karna241280@gmail.com>
2022-06-23 01:02:55 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/email/EmailNotification.java
@@ -0,0 +1,37 @@
+package ru.job4j.email;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    /**
+     * готовит данные к отправке и добавляет задачу по отправке в Executor (и запускает её)
+     */
+    public void emailTo(User user) {
+        String subject = "Notification " + user.getUsername() + " to email " + user.getEmail();
+        String body = "Add a new event to " + user.getUsername() + ".";
+        pool.submit(() -> send(subject, body, user.getEmail()));
+    }
+
+    /**
+     * shutdown() - Упорядоченное завершение работы,
+     * при котором ранее отправленные задачи выполняются, а новые задачи не принимаются
+     */
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+}
